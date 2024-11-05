35fa0c1bd278bbbeebcf2cd584d78c298abceefe
Rehan-Baig1294 <rehanbaig12@outlook.com>
2024-11-05 20:53:27 +0000

    flake8 fixes

--- life/life.py
+++ life/life.py
@@ -1,13 +1,13 @@
-import numpy as np
+import numpy as np # noqa d100
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
+ [0, 0, 0],
+ [1, 1, 1],
+ [0, 0, 0]]
     )
 
 glider_gun = np.array([
@@ -49,30 +49,40 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """Cellular Automata Game."""
+
+    def __init__(self, game, size):
+        """Construct method."""
+        game.board = np.zeros((size, size))
+
     def play(self):
+        """Playing the game."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Move in the game."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (neighbourcount[i, j] == 3
+                                         or (neighbourcount[i, j] == 2
+                                         and self.board[i, j])) else 0
 
     def __setitem__(self, key, value): 
+        """Set items."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Show in the game."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
